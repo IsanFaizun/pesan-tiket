89d6291f443ed45a1771f1fb2028bf9dc89dcc26
IsanFaizun <113765539+IsanFaizun@users.noreply.github.com>
2023-10-31 23:15:20 +0700

    Tambah Activity dan Fragment

---
+++ app/src/main/java/com/example/tugas09/CheckoutFragment.kt
@@ -0,0 +1,48 @@
+package com.example.tugas09
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
+import com.example.tugas09.databinding.FragmentCheckoutBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CheckoutFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CheckoutFragment : Fragment() {
+    private lateinit var binding: FragmentCheckoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentCheckoutBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            spinnerTiket.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {}
+                override fun onNothingSelected(p0: AdapterView<*>?) {}
+            }
+
+            btnBuy.setOnClickListener {
+                findNavController().navigateUp()
+            }
+        }
+    }
+}
